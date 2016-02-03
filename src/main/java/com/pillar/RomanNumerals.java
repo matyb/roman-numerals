a9f7f36d74c2068febfba680d6d1146b4cec5fc6
matyb <mathewkbentley@gmail.com>
2016-02-02 21:02:47 -0500

    RomanNumerals.getNumeral working for input of 9 (IX)

--- src/main/java/com/pillar/RomanNumerals.java
+++ src/main/java/com/pillar/RomanNumerals.java
@@ -1,6 +1,8 @@
 package com.pillar;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -20,6 +22,28 @@ public class RomanNumerals {
 	}
 	
 	public String getNumeral(Integer number) {
+		List<Entry<Integer,String>> tuple = findAdjacentNumerals(number); 
+		return toNumeral(number, tuple);	
+	}
+
+	private String toNumeral(Integer number, List<Entry<Integer, String>> tuple) {
+		Entry<Integer, String> lower = tuple.get(0);
+		if(tuple.size() == 1){
+			return lower.getValue();
+		} else {
+			Entry<Integer, String> upper = tuple.get(1);
+			if(upper.getKey() - lower.getKey() == number){
+				return lower.getValue() + upper.getValue();
+			}else if(upper.getKey() - number < lower.getKey()){
+				tuple = findAdjacentNumerals(lower.getKey() - 1);
+				return toNumeral(number, Arrays.asList(tuple.get(0), upper));
+			}else{
+				return String.format("%s%s%s", lower.getValue(),lower.getValue(),lower.getValue());
+			}
+		}
+	}
+
+	private List<Entry<Integer, String>> findAdjacentNumerals(Integer number) {
 		Entry<Integer, String> lower = null, upper = null;
 		for(Entry<Integer, String> entry : numeralByNumber.entrySet()){
 			if(number > entry.getKey()){
@@ -28,14 +52,10 @@ public class RomanNumerals {
 				upper = entry;
 				break;
 			}else{
-				return entry.getValue();
+				return Arrays.asList(entry);
 			}
 		}
-		if(upper.getKey() - lower.getKey() == number){
-			return lower.getValue() + upper.getValue();
-		}else{
-			return String.format("%s%s%s", lower.getValue(),lower.getValue(),lower.getValue());
-		}	
+		return Arrays.asList(lower, upper);
 	}
 	
 }
