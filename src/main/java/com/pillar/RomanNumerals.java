6fa57f56e95a46f97e7a614d16a15ef00db6bb82
matyb <mathewkbentley@gmail.com>
2016-02-02 20:26:01 -0500

    RomanNumerals.getNumeral for cases 1 and 3 working

--- src/main/java/com/pillar/RomanNumerals.java
+++ src/main/java/com/pillar/RomanNumerals.java
@@ -2,6 +2,7 @@ package com.pillar;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class RomanNumerals {
 
@@ -17,9 +18,24 @@ public class RomanNumerals {
 		numeralByNumber.put(1000, "M");
 		this.numeralByNumber = numeralByNumber;
 	}
-
+	
 	public String getNumeral(Integer number) {
-		return numeralByNumber.get(number);
+		Entry<Integer, String> lower = null, upper = null;
+		for(Entry<Integer, String> entry : numeralByNumber.entrySet()){
+			if(number > entry.getKey()){
+				lower = entry;
+			}else if(number < entry.getKey()){
+				upper = entry;
+				break;
+			}else{
+				return entry.getValue();
+			}
+		}
+		if(upper.getKey() - lower.getKey() == number){
+			return lower.getValue() + upper.getValue();
+		}else{
+			return String.format("%s%s%s", lower.getValue(),lower.getValue(),lower.getValue());
+		}	
 	}
 	
 }
--- src/test/java/com/pillar/RomanNumeralsTest.java
+++ src/test/java/com/pillar/RomanNumeralsTest.java
@@ -11,4 +11,24 @@ public class RomanNumeralsTest {
 		assertEquals("I", new RomanNumerals().getNumeral(1));
 	}
 	
+	@Test
+	public void number3IsNumeralIII() throws Exception {
+		assertEquals("III", new RomanNumerals().getNumeral(3));
+	}
+	
+	@Test
+	public void number9IsNumeralIX() throws Exception {
+		assertEquals("IX", new RomanNumerals().getNumeral(9));
+	}
+	
+	@Test
+	public void number1066IsNumeralMLXVI() throws Exception {
+		assertEquals("MLXVI", new RomanNumerals().getNumeral(1066));
+	}
+	
+	@Test
+	public void number1989IsNumeralMCMLXXXIX() throws Exception {
+		assertEquals("MCMLXXXIX", new RomanNumerals().getNumeral(1989));
+	}
+	
 }
