fe2075180635e138e29cc13f7f13ab29afc26dbd
matyb <mathewkbentley@gmail.com>
2016-02-03 21:55:19 -0500

    making comment less dramatic

--- src/test/java/com/pillar/conversions/romannumerals/RomanNumeralsConverterTest.java
+++ src/test/java/com/pillar/conversions/romannumerals/RomanNumeralsConverterTest.java
@@ -26,29 +26,23 @@ public class RomanNumeralsConverterTest {
 		cases.put("MCMLXXXIX", 1989);
 	}
 	
-	/**
-	 * <b>TODO: I KNOW THIS IS GENERALLY A BAD IDEA TO TEST > 1 THING IN ONE
-	 * UNIT TEST! but enforcing symmetry between conversions felt more valuable.<b>
-	 * oh and comments in tests are usually a code smell, but - i wanted to explain.
-	 */
+	// TODO: enforce symmetry without testing more than one thing in a method!
 	@Test
 	public void testNumberToNumeral() throws Exception {
 		RomanNumeralsConverter romanNumerals = new RomanNumeralsConverter();
 		for(Entry<String, Integer> entry : cases.entrySet()){
-			assertEquals(entry.getKey(), romanNumerals.from(entry.getValue()));
+			assertEquals("Converting ARABIC -> ROMAN failed.",
+					entry.getKey(), romanNumerals.from(entry.getValue()));
 		}
 	}
 	
-	/**
-	 * <b>TODO: I KNOW THIS IS GENERALLY A BAD IDEA TO TEST > 1 THING IN ONE
-	 * UNIT TEST! but enforcing symmetry between conversions felt more valuable.<b>
-	 * oh and comments in tests are usually a code smell, but - i wanted to explain.
-	 */
+	// TODO: enforce symmetry without testing more than one thing in a method!
 	@Test
 	public void testNumeralToNumber() throws Exception {
 		RomanNumeralsConverter romanNumerals = new RomanNumeralsConverter();
 		for(Entry<String, Integer> entry : cases.entrySet()){
-			assertEquals(entry.getValue(), romanNumerals.to(entry.getKey()));
+			assertEquals("Converting ROMAN -> ARABIC failed.",
+					entry.getValue(), romanNumerals.to(entry.getKey()));
 		}
 	}
 	
